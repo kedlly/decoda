312018441f04dc460e9f4f1be1ec6c6048b35237
kedlly <zhaoxh6525@163.com>
2021-05-11 09:57:38 +0800

    update

---
+++ xcode_test/test/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  decoda
+//
+//  Created by kedlly on 2021/5/11.
+//
+
+#include <iostream>
+#include "INetFrame.h"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    byte *p, *q;
+    Build(p, 0, q, 0);
+    return 0;
+}
